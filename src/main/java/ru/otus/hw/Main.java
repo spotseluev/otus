3b44b0b933bd82e7db293d8c35664c48ea3bff41
Sergey Potseluev <potseluev.s@gmail.com>
2024-04-09 01:43:28 +0300

    new test commit

--- src/main/java/ru/otus/hw/Main.java
+++ src/main/java/ru/otus/hw/Main.java
@@ -1,17 +1,62 @@
-package ru.otus.hw;
+package  ru.otus.hw;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+
+public class Main  {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        greetings();
+        checkSign(1, 5, 10);
+        selectCoulour();
+        compareNumbers();
+        addOrSubtractAndPrint(25,30, true);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void greetings() {
+        System.out.print("Hello\nWorld\nFrom\nJava\n");
+    }
+
+    public static void checkSign(int a, int b, int c) {
+        int result = a + b + c;
+        if (result >= 0) {
+            System.out.println("result is positive");
+
+        } else {
+            System.out.println("result is negative");
+        }
+    }
+
+
+    public static void selectCoulour() {
+
+        int data = 1;
+
+        if (data < 10) {
+
+            System.out.println("Red");
+        } else if (data <= 20 && data >= 10) {
+            System.out.println("Yellow");
+        } else {
+            System.out.println("Green");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 20;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+
+    }
+    public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment){
+        if (increment) {
+            System.out.println(initValue+delta);
+
+        } else {
+            System.out.println(initValue-delta);
         }
     }
 }
+
